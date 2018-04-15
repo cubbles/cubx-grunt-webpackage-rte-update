0c2371bd79a8fe13b6eacda221bf514ec054cb80
edwingamboa <edwingamboa355@gmail.com>
2018-04-15 11:46:56 -0500

    cubbles/cubbles-coder-devtools#6: Add task script

---
+++ tasks/cubx-grunt-webpackage-rte-update.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var inquirer = require('inquirer');
+var RteUpdater = require('cubx-webpackage-rte-update');
+
+module.exports = function (grunt) {
+  grunt.registerTask('_webpackage-updateRte', 'Update rte references in manifest file and in artifact files of a webpackage', function () {
+    var webpackagePath = grunt.config.get('param.src') || grunt.config.get('webpackagepath');
+    if (!webpackagePath) {
+      throw new Error('webpackagePath missed. Please define \'webpackagePath\' option .');
+    }
+    var rteUpdater = new RteUpdater(webpackagePath);
+
+    var options = {
+      questions: [
+        {
+          name: 'rteVersion',
+          type: 'input',
+          message: 'Provide the rte version to be used (e.g. 2.3.0, 3.0.0-SNAPHOT)',
+          validate: function (input) {
+            var versionRegExp = new RegExp('^(\\d+)(\\.[\\d]+)*(-SNAPSHOT)?$');
+            if (!versionRegExp.test(input)) {
+              throw new Error('Invalid Rte version (' + input + '). Please provide a valid value like 2.3.0 or 3.0.0-SNAPSHOT');
+            }
+          }
+        }
+      ]
+    };
+
+    var done = this.async();
+    inquirer.prompt(options.questions).then(function (result) {
+      rteUpdater.updateRteInWebpackage(result.rteVersion);
+      done();
+    });
+  });
+};
